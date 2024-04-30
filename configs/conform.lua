18730ebf3cd05f3caf6e497189529521c1553215
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2024-04-30 00:03:52 -0300

    feat: add code formatter, and linter

--- configs/conform.lua
+++ configs/conform.lua
@@ -1,15 +1,30 @@
 local options = {
   formatters_by_ft = {
+    css = { "prettier" },
+    html = { "prettier" },
+    json = { "prettier" },
+    yaml = { "prettier" },
+    graphql = { "prettier" },
+    markdown = { "prettier" },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescriptreact = { "prettier" },
+    svelte = { "prettier" },
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    python = { "isort", "black" },
+    c = { "clang_format" },
+    cpp = { "clang_format" },
+    c_sharp = { "csharpier" },
+    kotlin = { "ktlint" },
+    go = { "gofmt" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 require("conform").setup(options)
--- plugins/init.lua
+++ plugins/init.lua
@@ -9,6 +9,34 @@ return {
     end,
   },
 
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local lint = require "lint"
+
+      lint.linters_by_ft = {
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+        svelte = { "eslint_d" },
+        python = { "pylint" },
+        go = { "golangcilint", "revive" },
+        kotlin = { "ktlint" },
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
+
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
