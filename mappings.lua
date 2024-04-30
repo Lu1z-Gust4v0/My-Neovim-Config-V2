987375f27f7f815059814ace5820adb861cdb0af
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2024-04-29 22:14:06 -0300

    feat: add move lines mappings

--- mappings.lua
+++ mappings.lua
@@ -6,5 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
